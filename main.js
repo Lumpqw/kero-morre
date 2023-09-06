f7843458dd292afd3c5042b517d988b0d8ebdf77
Lumpqw <131711531+Lumpqw@users.noreply.github.com>
2023-09-06 16:46:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas.addEventListener("mousedown", myMouseDown);
+function myMouseDown(e)
+{
+    color = document.getElementById("color").value;
+    widthLine = document.getElementById("widthLine").value;
+    mouseEvent = "mouseDown";
+}
+
+canvas.addEventListener("mouseup", myMouseUp);
+function myMouseUp(e)
+{
+    mouseEvent = "mouseUp";
+}
+
+canvas.addEventListener("mousemove", myMouseMove);
+function myMouseMove(e)
+{
+    PositionMouseX = e.clientX - canvas.offsetLeft;
+    PositionMouseY = e.clientY - canvas.offsetTop;
+
+    if (mouseEvent == "mouseDown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = widthLine;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + lastPositionX + "y = " + lastPositionY);
+        ctx.moveTo(lastPositionX, lastPositionY);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x = " + PositionMouseX + "y = " + PositionMouseY);
+        ctx.lineTo(PositionMouseX, PositionMouseY);
+        ctx.stroke()
+    }
+
+    lastPositionX = PositionMouseX
+    lastPositionY = PositionMouseY
+}
